3932b7552363ba7fe20289990abf2598a6845f6f
Carter Page <cpage@pobox.com>
2011-04-09 16:24:09 -0400

    Initial classes and directory structure.

---
+++ src/main/java/org/skyscreamer/yoga/Yoga.java
@@ -0,0 +1,11 @@
+package org.skyscreamer.yoga;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cpage
+ * Date: 4/9/11
+ * Time: 4:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Yoga {
+}
