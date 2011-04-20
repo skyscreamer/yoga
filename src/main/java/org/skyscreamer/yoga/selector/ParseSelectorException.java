6891ff56cc9ebcf12747875b04f3d995c97be241
Carter Page <cpage@pobox.com>
2011-04-20 08:04:15 +0800

    Changed ParseSelectorException to caught exception to differentiate between config and argument errors Changed tests to JUnit4

--- src/main/java/org/skyscreamer/yoga/selector/ParseSelectorException.java
+++ src/main/java/org/skyscreamer/yoga/selector/ParseSelectorException.java
@@ -1,7 +1,15 @@
 package org.skyscreamer.yoga.selector;
 
-public class ParseSelectorException extends RuntimeException
+public class ParseSelectorException extends Exception
 {
-    private static final long serialVersionUID = -5307328628794516456L;
+    private static final long serialVersionUID = -5307328628794516457L;
+
+    public ParseSelectorException() {
+        super();
+    }
+
+    public ParseSelectorException(String s) {
+        super(s);
+    }
 }
 
