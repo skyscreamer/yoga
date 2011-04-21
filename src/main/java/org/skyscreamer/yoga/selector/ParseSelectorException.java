984f670c59b7be43bae979ef45e69dc7bfb1513a
CORBY PAGE <corby@CORBY-PAGEs-MacBook-Pro.local>
2011-04-22 04:43:18 +0800

    Removed unused classes

--- src/main/java/org/skyscreamer/yoga/selector/ParseSelectorException.java
+++ src/main/java/org/skyscreamer/yoga/selector/ParseSelectorException.java
@@ -4,12 +4,14 @@ public class ParseSelectorException extends Exception
 {
     private static final long serialVersionUID = -5307328628794516457L;
 
-    public ParseSelectorException() {
+    public ParseSelectorException()
+    {
         super();
     }
 
-    public ParseSelectorException(String s) {
-        super(s);
+    public ParseSelectorException( String s )
+    {
+        super( s );
     }
 }
 
