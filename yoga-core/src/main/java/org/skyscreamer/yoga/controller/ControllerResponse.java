80c16d180d6bc4c5bd3e0d87a96fdd4fdaf11bb1
Corby Page <cepage@gmail.com>
2011-05-30 22:55:28 -0500

    Binding the selector at the beginning of the controller method to enable DTO population

---
+++ yoga-core/src/main/java/org/skyscreamer/yoga/controller/ControllerResponse.java
@@ -0,0 +1,12 @@
+package org.skyscreamer.yoga.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: corby
+ * Date: 5/30/11
+ * Time: 10:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ControllerResponse
+{
+}
