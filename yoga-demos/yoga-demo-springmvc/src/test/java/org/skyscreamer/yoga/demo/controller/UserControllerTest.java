4378dd29a36e83d734cb20c23ed3a7d753113d39
Corby Page <cepage@gmail.com>
2011-07-10 16:52:44 -0500

    Fixed tests

--- yoga-demos/yoga-demo-springmvc/src/test/java/org/skyscreamer/yoga/demo/controller/UserControllerTest.java
+++ yoga-demos/yoga-demo-springmvc/src/test/java/org/skyscreamer/yoga/demo/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,16 +12,15 @@ import org.junit.Test;
 /**
  * Created by IntelliJ IDEA.
  * User: Carter Page
- * Date: 4/11/11
- * Time: 6:05 PM
  */
 public class UserControllerTest extends AbstractTest {
     @Test
     public void testGetUser() throws Exception {
         JSONObject data = getJSONObject( "/user/2", null );
         Assert.assertEquals("Corby Page", data.getString("name"));
-        Assert.assertEquals(2, data.getLong("id"));
-        Assert.assertEquals(2, data.length());
+        Assert.assertEquals(3, data.length());
+        Assert.assertEquals(2, data.getLong( "id" ));
+        Assert.assertEquals( "/user/2", data.getString( "href" ) );
     }
 
     @Test
@@ -28,22 +28,22 @@ public class UserControllerTest extends AbstractTest {
         JSONArray data = getJSONArray( "/user", null );
         Assert.assertEquals(2, data.length());
         JSONObject carter = data.getJSONObject( 0 );
-        Assert.assertEquals(2, carter.length());
-        Assert.assertEquals("Carter Page", carter.getString("name"));
-        Assert.assertEquals(1, carter.getLong("id"));
+        Assert.assertEquals(3, carter.length());
+        Assert.assertEquals("Carter Page", carter.getString( "name" ));
+        Assert.assertEquals(1, carter.getLong( "id" ));
+        Assert.assertEquals( "/user/1", carter.getString( "href" ) );
         JSONObject corby = data.getJSONObject( 1 );
         Assert.assertEquals("Corby Page", corby.getString("name"));
-        Assert.assertEquals(2, corby.getLong("id"));
-        Assert.assertEquals(2, corby.length());
+        Assert.assertEquals(3, corby.length());
+        Assert.assertEquals(2, corby.getLong( "id" ));
+        Assert.assertEquals( "/user/2", corby.getString( "href" ) );
     }
 
     @Test
     public void testGetUserWithSelector() throws Exception {
         Map<String, String> params = Collections.singletonMap("selector", ":(isFriend)");
         JSONObject data = getJSONObject( "/user/1", params );
-        Assert.assertEquals(3, data.length());
-        Assert.assertEquals("Carter Page", data.getString("name"));
-        Assert.assertEquals(1, data.getLong("id"));
+        checkCarter( data );
         Assert.assertFalse(data.getBoolean("isFriend"));
     }
 
@@ -51,45 +51,37 @@ public class UserControllerTest extends AbstractTest {
     public void testGetUserWithFriends() throws Exception {
         Map<String, String> params = Collections.singletonMap("selector", ":(friends)");
         JSONObject data = getJSONObject( "/user/1", params );
-        Assert.assertEquals(3, data.length());
-        Assert.assertEquals("Carter Page", data.getString("name"));
-        Assert.assertEquals(1, data.getLong("id"));
+        checkCarter( data );
         JSONArray friends = data.getJSONArray("friends");
         Assert.assertEquals(1, friends.length());
         JSONObject friend = friends.getJSONObject(0);
-        Assert.assertEquals(2, friend.length());
-        Assert.assertEquals("Corby Page", friend.getString("name"));
+        Assert.assertEquals(3, friend.length());
+        Assert.assertEquals("Corby Page", friend.getString( "name" ));
+        Assert.assertEquals( "/user/2", friend.getString( "href" ) );
     }
 
     @Test
     public void testGetUserWithFriendsAndSelector() throws Exception {
         Map<String, String> params = Collections.singletonMap("selector", ":(friends:(isFriend))");
         JSONObject data = getJSONObject( "/user/1", params );
-        Assert.assertEquals(3, data.length());
-        Assert.assertEquals("Carter Page", data.getString("name"));
-        Assert.assertEquals(1, data.getLong("id"));
+        checkCarter( data );
         JSONArray friends = data.getJSONArray("friends");
         Assert.assertEquals(1, friends.length());
         JSONObject friend = friends.getJSONObject(0);
-        Assert.assertEquals(3, friend.length());
-        Assert.assertEquals("Corby Page", friend.getString("name"));
-        Assert.assertEquals(2, friend.getLong("id"));
+        checkCorby( friend );
         Assert.assertFalse(friend.getBoolean("isFriend"));
     }
 
     @Test
-    public void testDeepDiveSelector() throws Exception {
+    public void testDeepDiveSelector() throws Exception
+    {
         Map<String, String> params = Collections.singletonMap("selector", ":(friends:(favoriteArtists:(albums:(songs))))");
         JSONObject data = getJSONObject( "/user/1", params );
-        Assert.assertEquals(3, data.length());
-        Assert.assertEquals("Carter Page", data.getString("name"));
-        Assert.assertEquals(1, data.getLong("id"));
+        checkCarter( data );
         JSONArray friends = data.getJSONArray("friends");
         Assert.assertEquals(1, friends.length());
         JSONObject friend = friends.getJSONObject(0);
-        Assert.assertEquals(3, friend.length());
-        Assert.assertEquals("Corby Page", friend.getString("name"));
-        Assert.assertEquals(2, friend.getLong("id"));
+        checkCorby( friend );
         JSONArray favoriteArtists = friend.getJSONArray("favoriteArtists");
         Assert.assertEquals(2, favoriteArtists.length());
         JSONObject prince = "Prince".equals(favoriteArtists.getJSONObject(0).getString("name"))
@@ -98,5 +90,21 @@ public class UserControllerTest extends AbstractTest {
         JSONObject prince1999 = albums.getJSONObject(0);
         JSONObject corvette = prince1999.getJSONArray("songs").getJSONObject(1);
         Assert.assertEquals("Little Red Corvette", corvette.getString("title"));
+    }
+
+    private void checkCorby( JSONObject friend ) throws JSONException
+    {
+        Assert.assertEquals( 4, friend.length() );
+        Assert.assertEquals("Corby Page", friend.getString( "name" ));
+        Assert.assertEquals(2, friend.getLong( "id" ));
+        Assert.assertEquals( "/user/2", friend.getString( "href" ) );
+    }
+
+    private void checkCarter( JSONObject data ) throws JSONException
+    {
+        Assert.assertEquals( 4, data.length() );
+        Assert.assertEquals( "Carter Page", data.getString( "name" ) );
+        Assert.assertEquals( 1, data.getLong( "id" ) );
+        Assert.assertEquals( "/user/1", data.getString( "href" ) );
     }
 }
