3c4505f42673c71cc2651ad7db2735cf92c0c9a9
sduskis <sduskis@gmail.com>
2011-09-27 22:34:09 -0400

    initial add of metadata "stuff."  Next, add some links from the data and add populator metaData

--- yoga-core/src/main/java/org/skyscreamer/yoga/metadata/MapMetaDataServiceImpl.java
+++ yoga-core/src/main/java/org/skyscreamer/yoga/metadata/MapMetaDataServiceImpl.java
@@ -1,50 +1,153 @@
 package org.skyscreamer.yoga.metadata;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import org.skyscreamer.yoga.mapper.ResultTraverser;
+import org.skyscreamer.yoga.selector.Core;
+import org.skyscreamer.yoga.util.NameUtil;
 
 public class MapMetaDataServiceImpl implements MetaDataService
 {
 
-   private Map<String, Class<?>> _map;
+   private Map<String, Class<?>> _typeMappings;
+   private Map<Class<?>, String> _typeToStringMap = new HashMap<Class<?>, String>();
 
-   public void setMap(Map<String, Class<?>> map)
+   private String rootMetaDataUrl;
+
+   public String getRootMetaDataUrl()
    {
-      this._map = map;
+      return rootMetaDataUrl;
+   }
+
+   public void setRootMetaDataUrl(String rootMetaDataUrl)
+   {
+      this.rootMetaDataUrl = rootMetaDataUrl;
+   }
+
+   public void setTypeMappings(Map<String, Class<?>> map)
+   {
+      this._typeMappings = map;
+      _typeToStringMap.clear();
+      for (Entry<String, Class<?>> entry : map.entrySet())
+      {
+         _typeToStringMap.put( entry.getValue(), entry.getKey() );
+      }
    }
 
    @Override
    public Map<String, Class<?>> getTypeMappings()
    {
-      return _map;
+      return _typeMappings;
    }
 
    @Override
    public Class<?> getTypeForName(String name)
    {
-      return _map.get(name);
+      return _typeMappings.get( name );
    }
 
    /**
     * given a type, get a name. This takes subclassing into consideration. For
-    * now, this will return the first subclass match to the type, not the closest
+    * now, this will return the first subclass match to the type, not the
+    * closest
     */
    @Override
    public String getNameForType(Class<?> type)
    {
-      String closeMatch = null;
-      for (Entry<String, Class<?>> entry : _map.entrySet())
+      if (_typeToStringMap.containsKey( type ))
       {
-         if(entry.getValue() == type)
+         return _typeToStringMap.get( type );
+      }
+
+      for (Entry<String, Class<?>> entry : _typeMappings.entrySet())
+      {
+         if (entry.getValue().isAssignableFrom( type ))
          {
             return entry.getKey();
          }
-         if (entry.getValue().isAssignableFrom(type))
+      }
+      return null;
+   }
+
+   @Override
+   public TypeMetaData getMetaData(String name, String suffix)
+   {
+      return getMetaData( getTypeForName( name ), suffix );
+   }
+
+   @Override
+   public TypeMetaData getMetaData(Class<?> type, String suffix)
+   {
+      TypeMetaData result = new TypeMetaData();
+      result.setName( NameUtil.getFormalName( type ) );
+      for (PropertyDescriptor property : ResultTraverser.getReadableProperties( type ))
+      {
+         Class<?> propertyType = property.getPropertyType();
+         PropertyMetaData propertyMetaData = new PropertyMetaData();
+         result.getPropertyMetaData().add( propertyMetaData );
+         if (ResultTraverser.isPrimitive( propertyType ))
          {
-            closeMatch = entry.getKey();
+            setValues( property, propertyType, propertyMetaData );
+         }
+         else if (Iterable.class.isAssignableFrom( propertyType ))
+         {
+            Class<?> collectionValueType = getIterableType( property.getReadMethod()
+                  .getGenericReturnType() );
+            String typeName = NameUtil.getFormalName( collectionValueType ) + "[]";
+            setValues( property, typeName, propertyMetaData );
+            addHref( propertyMetaData, collectionValueType, suffix );
+         }
+         else
+         {
+            setValues( property, NameUtil.getFormalName( propertyType ), propertyMetaData );
+            addHref( propertyMetaData, propertyType, suffix );
          }
       }
-      return closeMatch;
+      return result;
+   }
+
+   protected void addHref(PropertyMetaData propertyMetaData, Class<?> propertyType, String suffix)
+   {
+      String nameForType = getNameForType( propertyType );
+      if (nameForType != null)
+      {
+         propertyMetaData.setHref( getRootMetaDataUrl() + nameForType + "." + suffix );
+      }
+   }
+
+   protected Class<?> getIterableType(Type type)
+   {
+      if (type instanceof ParameterizedType)
+      {
+         ParameterizedType zType = (ParameterizedType) type;
+         return (Class<?>) zType.getActualTypeArguments()[0];
+      }
+      return null;
+   }
+
+   protected void setValues(PropertyDescriptor property, Class<?> propertyType,
+         PropertyMetaData propertyMetaData)
+   {
+      String type = propertyType == String.class ? "String" : propertyType.getName();
+      setValues( property, type, propertyMetaData );
+   }
+
+   protected void setValues(PropertyDescriptor property, String propertyType,
+         PropertyMetaData propertyMetaData)
+   {
+      propertyMetaData.setName( property.getName() );
+      propertyMetaData.setType( propertyType );
+      propertyMetaData.setIsCore( isCore( property ) );
+   }
+
+   protected boolean isCore(PropertyDescriptor property)
+   {
+      return property.getReadMethod().isAnnotationPresent( Core.class );
    }
 
 }
