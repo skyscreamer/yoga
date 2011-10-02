946592f2e2b70cb094619bd2bbfad771464d52f3
Carter Page <carter@skyscreamer.org>
2011-10-02 15:08:41 -0400

    Enable metadata for Spring MVC.

---
+++ yoga-demos/yoga-demo-springmvc/src/main/java/org/skyscreamer/yoga/demo/test/controller/MetaDataController.java
@@ -0,0 +1,28 @@
+package org.skyscreamer.yoga.demo.test.controller;
+
+import org.skyscreamer.yoga.metadata.MetaDataService;
+import org.skyscreamer.yoga.metadata.TypeMetaData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/metadata")
+public class MetaDataController
+{
+   @Autowired
+   MetaDataService metaDataService;
+
+   @RequestMapping("/{type}")
+   public TypeMetaData getTypeMetaData(@PathVariable("type") String type,
+         HttpServletRequest request)
+   {
+      String uri = request.getRequestURI();
+      String parts[] = uri.split( "\\." );
+      return metaDataService.getMetaData( type, parts[parts.length - 1] );
+   }
+
+}
