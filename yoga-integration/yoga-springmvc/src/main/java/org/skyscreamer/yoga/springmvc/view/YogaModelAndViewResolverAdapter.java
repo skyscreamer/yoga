0cf566ebf62a14b5c7e454269f3dbf3ee3b352f0
Solomon Duskis <sduskis@gmail.com>
2012-11-14 23:11:27 -0500

    more comments

--- yoga-integration/yoga-springmvc/src/main/java/org/skyscreamer/yoga/springmvc/view/YogaModelAndViewResolverAdapter.java
+++ yoga-integration/yoga-springmvc/src/main/java/org/skyscreamer/yoga/springmvc/view/YogaModelAndViewResolverAdapter.java
@@ -14,40 +14,48 @@ import org.springframework.web.servlet.mvc.annotation.ModelAndViewResolver;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
+ * <p>
  * This is a bit of a hack to create ModelAndViewResolver as an adapter for a
  * ViewResolver, namely the ContentNegotiationViewResolver. There's plenty of
  * room for improvement here, especially since there isn't much logic in this
  * adapter. There's plenty of interesting additions that one can use to extend
  * or replace this class, including additional filtering logic.
+ * </p>
+ * 
+ * <p>
+ * You can log an issue or reach out to the development team about any
+ * extensions you might be considering
+ * </p>
  * 
  * @author solomon.duskis
  * 
  */
 public class YogaModelAndViewResolverAdapter implements ModelAndViewResolver
 {
-	ViewResolver viewResolver;
+    ViewResolver viewResolver;
 
-	public void setViewResolver(ViewResolver viewResolver)
-	{
-		this.viewResolver = viewResolver;
-	}
+    public void setViewResolver(ViewResolver viewResolver)
+    {
+        this.viewResolver = viewResolver;
+    }
 
-	@Override
-	@SuppressWarnings("rawtypes")
-	public ModelAndView resolveModelAndView(Method handlerMethod,
-			Class handlerType, Object returnValue,
-			ExtendedModelMap implicitModel, NativeWebRequest webRequest)
-	{
-		try
-		{
-			HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-			Locale locale = RequestContextUtils.getLocale(request);
-			View view = viewResolver.resolveViewName("yoga", locale);
-			return view == null ? ModelAndViewResolver.UNRESOLVED : new ModelAndView(view, "value", returnValue);
-		} catch (Exception e)
-		{
-			throw new RuntimeException("could not process request", e);
-		}
-	}
+    @Override
+    @SuppressWarnings("rawtypes")
+    public ModelAndView resolveModelAndView(Method handlerMethod,
+            Class handlerType, Object returnValue,
+            ExtendedModelMap implicitModel, NativeWebRequest webRequest)
+    {
+        try
+        {
+            HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+            Locale locale = RequestContextUtils.getLocale(request);
+            View view = viewResolver.resolveViewName("yoga", locale);
+            return view == null ? ModelAndViewResolver.UNRESOLVED : new ModelAndView(view, "value", returnValue);
+        } 
+        catch (Exception e)
+        {
+            throw new RuntimeException("could not process request", e);
+        }
+    }
 
 }
