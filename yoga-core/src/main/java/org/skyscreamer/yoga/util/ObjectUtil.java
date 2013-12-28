a5198855887d112d5dd218760842a08b8166d57b
Kenton Gray <kentongray@gmail.com>
2013-12-27 19:07:45 -0600

    Consider Date primitive
    This allows jersey/jax-ws to handle date representation (by default
    epoch)

--- yoga-core/src/main/java/org/skyscreamer/yoga/util/ObjectUtil.java
+++ yoga-core/src/main/java/org/skyscreamer/yoga/util/ObjectUtil.java
@@ -1,11 +1,13 @@
 package org.skyscreamer.yoga.util;
 
+import java.util.Date;
+
 public class ObjectUtil
 {
     public static boolean isPrimitive( Class<?> clazz )
     {
         return clazz.isPrimitive() || clazz.isEnum() || Number.class.isAssignableFrom( clazz )
                 || String.class.isAssignableFrom( clazz ) || Boolean.class.isAssignableFrom( clazz )
-                || Character.class.isAssignableFrom( clazz );
+                || Character.class.isAssignableFrom( clazz ) || Date.class.isAssignableFrom( clazz );
     }
 }
